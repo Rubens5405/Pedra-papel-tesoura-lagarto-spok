8b68d88b8e7ea5d2f9f7078bd8cc85cf95af36a5
Rubens Araujo <rubensaraujobo@gmail.com>
2026-03-05 18:42:05 -0300

    versus_cpu
    Adicionado a função versus_cpu para melhor compatibilidade com o menu, e outras pequenas correções.

--- jogar.py
+++ jogar.py
@@ -1,34 +1,50 @@
-from oponentes import Sheldon, Monarca, dialogo_especial
-from jogo import Jogo
-import random
+def versus_cpu(): 
+    from oponentes import Sheldon, Monarca, Sósia, Spock, dialogo_especial
+    from jogo import Jogo
+    import random
 
-escolhas = ["pedra", "papel", "tesoura", "lagarto", "spok"]
-oponentes = [Sheldon, Monarca]
+    escolhas = ["pedra", "papel", "tesoura", "lagarto", "spock"]
+    oponentes = ["Sheldon", "Monarca", "Spock", "Sósia"]
 
-rival = random.choice(oponentes)
+    rival = random.choice(oponentes)
+    pontos_jogador = 0
+    pontos_rival = 0
+    jogada_rival = random.choice(escolhas)
 
-rodada = 1
-pontos_jogador = 0
-pontos_rival = 0
+    def verificação():
+        escolha = input("Escolha entre Pedra, Papel, Tesoura, Lagarto ou Spock: ").lower()
+        while escolha not in escolhas:
+            print("Opção inválida.")
+            escolha = input("Tente novamente: ").lower()
+        return escolha
 
-for i in range(5):
-    opcao = input("Escolha Pedra, Papel, Tesoura, Lagarto ou Spok: ").lower()
-    if rival == Sheldon:
-        jogada_rival = Sheldon(rodada)
-    elif rival == Monarca:
-        jogada_rival = Monarca(opcao)
+    for rodada in range(5):
+        opcao = verificação()
 
-    print(f"{rodada}ª rodada:")
-    resultado = Jogo(opcao, jogada_rival)
-    if resultado == "Você venceu!":
-        pontos_jogador += 1
-    elif resultado == "Você perdeu!":
-        pontos_rival += 1
-    print(resultado)
-    print(f"{pontos_jogador}  X  {pontos_rival} \n")
-    rodada +=1
-    dialogo_especial(rodada, rival, pontos_jogador, pontos_rival)
+        #Condição de Rival
+        if rival == "Sheldon":
+            jogada_rival = Sheldon(rodada+1)
+    
+        resultado = Jogo(opcao, jogada_rival)
+        if resultado == "Você venceu!":
+            pontos_jogador += 1
+        elif resultado == "Você perdeu!":
+            pontos_rival += 1
 
-print(f"Seu oponente era: {rival.__name__}")
-print(f"\nVocê  X  {rival.__name__} \n  {pontos_jogador}   X   {pontos_rival}")
+        print(f"{rodada + 1}ª rodada:")
+        print(resultado)
+        print(f"{pontos_jogador}  X  {pontos_rival}")
+        print(f"Seu opononente jogou: {jogada_rival}")
+        dialogo_especial(rodada + 1, rival, pontos_jogador, pontos_rival)
+        print(" ")
 
+        #Condição de Rival
+        if rival == "Monarca":
+            jogada_rival = Monarca(opcao)
+        if rival == "Sósia":
+            jogada_rival = Sósia(opcao)
+        if rival == "Spock":
+            jogada_rival = Spock(pontos_jogador, pontos_rival)
+
+    print(f"Seu oponente era: {rival}")
+    print(f"\nVocê  X  {rival} \n  {pontos_jogador}   X   {pontos_rival}")
