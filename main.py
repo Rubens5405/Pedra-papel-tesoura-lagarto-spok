140cc9e2ea9423c718e63d20b6daf4e9a59a2b65
Rubens Araujo <rubensaraujobo@gmail.com>
2026-03-11 23:48:47 -0300

    Add player versus mode and improve game options

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from modo_história import introdução
 from jogar import versus_cpu
+from versus import versus_player
 
-print("Bem-vindo, escolha seu modo de jogo:")
+print("Escolha seu modo de jogo:")
 
 while True:
-    opcoes = input("Modo História - 1 \nContra o Computador - 2\nVersus Local -"\
-" Em construção \nSair do jogo - 4")
+    opcoes = input("\nModo História (Somente introdução) - 1 \nContra o Computador - 2\nVersus Local -"\
+" 3 \nSair do jogo - 4\n")
     
     if opcoes == "1":
-        print ("Segure shift para pular a introdução.")
         introdução()
 
-    elif opcoes == "2": 
+    elif opcoes == "2":
         versus_cpu()
+
+    elif opcoes =="3":
+        versus_player()
 
     elif opcoes == "4":
         break
